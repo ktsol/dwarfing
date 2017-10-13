d609bba00dbfaebd8a4bbc56ee216585e3794a50
Rumato Estorsky <rumatoest@users.noreply.github.com>
2017-10-13 19:54:23 +0300

    Add ccminer hr support

--- tools.sh
+++ tools.sh
@@ -43,7 +43,7 @@ check_miner_for() {
 }
 
 read_hr() {
-    read_miner 20 | grep -m 1 -oPi 'Total Speed:\s+\K(\d+)(?=(\.\d+)*\s.*)'
+    read_miner 100 | tac - | grep -m 1 -oPi '(Total Speed:\s+\K(\d+)(?=(\.\d+)*\s.*)|accepted:[^\)]+\), \K(\d+)(?=(\.\d+)*\s.*))'
 }
 
 #Hash rate less that provided
